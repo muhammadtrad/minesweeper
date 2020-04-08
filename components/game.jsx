71f8ba4e3dc2a403d1eb6209741c20280eaf135a
xmagnetisx <muhammad.trad@yahoo.com>
2020-04-07 22:52:58 -0700

    add game component

--- components/game.jsx
+++ components/game.jsx
@@ -1,2 +1,24 @@
 import React from 'react';
 
+class Game extends React.Component{
+    constructor(props){
+        this.state = { board: new Minesweeper.board};
+        this.updateGame = updateGame.bind(this);
+    }
+
+    updateGame(){
+
+    }
+
+
+    render(){
+
+        return(
+            <div>
+                <Board board={this.state.board, this.updateGame} /> 
+            </div>
+        );
+    }
+}
+
+export default Game;
