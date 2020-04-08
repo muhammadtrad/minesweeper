d5b86ff9a33bb6c95ec9b58dd72bd54f09818dc6
xmagnetisx <muhammad.trad@yahoo.com>
2020-04-08 01:41:15 -0700

    add board component

--- components/board.jsx
+++ components/board.jsx
@@ -7,9 +7,15 @@ class Board extends React.Component{
     }
 
     render(){
+        let content = <div></div>;
+        content += <h1>Hello</h1>;
+        // const output = this.props.board.map((elem, idx) =>{
+        //     content.innerHTML += <div key={idx}>{elem}</div>;
+        // });
         return(
             <div>
-                <p>T</p>
+                {content}
+                <h1>hello</h1>
             </div>
         );
     }
